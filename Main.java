f66527298478dca289cbeed54f98351e02787845
priyanth1 <81808832+priyanth1@users.noreply.github.com>
2021-05-07 22:25:04 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        byte  number = 10 ;
+        byte  number = 100 ;
         System.out.print(number);
     }
 }
